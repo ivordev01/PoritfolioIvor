2f6603ebb10313a809049336674618ea30980ab7
ivordev01 <rosa.ivor1721@gmail.com>
2026-03-17 12:15:45 -0300

    Commit 3: Project Stylization

--- app/index.tsx
+++ app/index.tsx
@@ -1,34 +1,57 @@
-import { StyleSheet, Text, View } from "react-native";
+import { ImageBackground, StyleSheet, Text, View } from "react-native";
 
 export default function HomePage() {
   return (
-    <View style={styles.container}>
+    <View style={styles.body}>
+      <ImageBackground source={require("../images/DSC06549 copy2.png")} style={styles.backgroundImage}/>
+      <View style={styles.opacityMask}/>
       <View style={styles.main}>
-        <Text style={styles.title}>Hello World</Text>
-        <Text style={styles.subtitle}>This is the first page of your app.</Text>
+        <Text style={styles.title}>Ivor Rosa</Text>
+        <Text style={styles.subtitle}>Memories out of frames</Text>
       </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+
+  body:
+  {
     flex: 1,
     alignItems: "center",
-    padding: 24,
   },
-  main: {
+
+  backgroundImage:
+  {
+    ...StyleSheet.absoluteFillObject,
+  },
+
+  opacityMask:
+  {
+    ...StyleSheet.absoluteFillObject,
+    backgroundColor: "#000000",
+    opacity: 0.5
+  },
+
+  main:
+  {
     flex: 1,
     justifyContent: "center",
     maxWidth: 960,
     marginHorizontal: "auto",
   },
-  title: {
+
+  title:
+  {
     fontSize: 64,
     fontWeight: "bold",
+    color: "#FFFFFF"
   },
-  subtitle: {
+
+  subtitle:
+  {
     fontSize: 36,
-    color: "#38434D",
+    color: "#f0f0f0",
   },
+
 });
